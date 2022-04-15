12f15bb1b0cc7aa3a9b52a310f56485f42b74e8b
MinhT_Toanhht <phammanhtoanhht933@gmail.com>
2022-04-15 11:25:38 +0700

    add

--- src/java/dto/CategoriesDTO.java
+++ src/java/dto/CategoriesDTO.java
@@ -10,5 +10,34 @@ package dto;
  * @author pc
  */
 public class CategoriesDTO {
+    String name;
+    String ID;
+
+    public CategoriesDTO(String name, String ID) {
+        this.name = name;
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoriesDTO{" + "name=" + name + ", ID=" + ID + '}';
+    }
+    
     
 }
--- src/java/dto/plantDTO.java
+++ src/java/dto/plantDTO.java
@@ -5,10 +5,71 @@
  */
 package dto;
 
+import java.util.Date;
+
 /**
  *
  * @author pc
  */
 public class plantDTO {
+    int ID;
+    String name;
+    float price;
+    String CateID;
+    Date createDate;
+
+    public plantDTO(int ID, String name, float price, String CateID, Date createDate) {
+        this.ID = ID;
+        this.name = name;
+        this.price = price;
+        this.CateID = CateID;
+        this.createDate = createDate;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getCateID() {
+        return CateID;
+    }
+
+    public void setCateID(String CateID) {
+        this.CateID = CateID;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    @Override
+    public String toString() {
+        return "plantDTO{" + "ID=" + ID + ", name=" + name + ", price=" + price + ", CateID=" + CateID + ", createDate=" + createDate + '}';
+    }
+    
     
 }
